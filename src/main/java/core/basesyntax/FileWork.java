b0d18f3f39f160a64be9b9f9f6497c79a3fbe373
Mateusz Lubiński <mateuszlubinski@hotmail.com>
2025-07-23 12:39:31 +0200

    Update FileWork.java
    add solution for word filtering

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,27 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        try {
+            return getWordsStartingWithW(fileName);
+        } catch (IOException e) {
+            throw new RuntimeException("There was a problem during reading file from path: "
+                    + fileName, e);
+        }
+
+    }
+
+    private static String[] getWordsStartingWithW(String fileName) throws IOException {
+        return Files.readAllLines(Path.of(fileName)).stream().map(line -> line.split("\\W+"))
+                .flatMap(Stream::of)
+                .map(String::toLowerCase)
+                .filter(word -> word.startsWith("w"))
+                .sorted()
+                .toArray(String[]::new);
     }
 }
